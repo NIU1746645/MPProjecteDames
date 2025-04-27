faa8faf1f7b547ae8fe9403d236a941aefcbd5b7
NIU1746645 <1746645@uab.cat>
2025-04-27 14:11:27 +0200

    Create posicio.hpp

---
+++ posicio.hpp
@@ -0,0 +1,37 @@
+#ifndef POSICIO_H
+#define POSICIO_H
+
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+const int N_FILES = 8;
+const int N_COLUMNES = 8;
+
+class Posicio
+{
+public:
+    Posicio() : m_fila(-1), m_columna(-1) {};
+    Posicio(int fila, int columna) { m_fila = fila; m_columna = columna; };
+    int getFila() const { return m_fila; };
+    int getColumna() const { return m_columna; };
+    void setPosicio(int fila, int columna) { m_fila = fila; m_columna = columna; };
+    string toString() const;
+    void fromString(const string& pos);
+
+    Posicio(const string& posicio) { fromString(posicio); }
+
+    bool operator==(const Posicio& posicio) const;
+
+
+private:
+    int m_fila, m_columna;
+};
+
+ifstream& operator>>(ifstream& fitxer, Posicio& posicio);
+ofstream& operator<<(ofstream& fitxer, Posicio& posicio);
+
+#endif // POSICIO_H
