7d9ee7df88d1c9da7e1434dd4a8825e555925aeb
foix <1751666@uab.cat>
2025-04-27 18:36:32 +0200

    arreglos de fitxa.cpp

--- fitxa.cpp
+++ fitxa.cpp
@@ -1,59 +1,66 @@
 #include "fitxa.h"
 
-char Fitxa::toChar() const 
+char Fitxa::toChar() const
 {
     char tornar = ' ';
 
-    if (m_tipus == TIPUS_EMPTY)
+    switch (m_tipus)
+    {
+    case TIPUS_EMPTY:
         tornar = ' ';
-    else 
-        if (m_tipus == TIPUS_NORMAL)
-        {
-            if (m_color == COLOR_BLANC)
-                tornar = 'B';
-            else
-                tornar = 'N';
-        }
+        break;
+
+    case TIPUS_NORMAL:
+        if (m_color == COLOR_BLANC)
+            tornar = 'B';
         else
-        {
-            if (m_color == COLOR_BLANC)
-                tornar = 'D';
-            else
-                tornar = 'R';
-        }
-    
+            tornar = 'N';
+        break;
+
+    case TIPUS_DAMA:
+        if (m_color == COLOR_BLANC)
+            tornar = 'D';
+        else
+            tornar = 'R';
+        break;
+    }
+
     return tornar;
 }
 
-void Fitxa::fromChar(char c) 
+void Fitxa::fromChar(char c)
 {
-    if (c == ' ') 
+    switch (c)
     {
+
+    case ' ':
         m_tipus = TIPUS_EMPTY;
         m_color = COLOR_BLANC;
-    }
-    else
-        if (c == 'B') 
-        {
+        break;
+
+    case 'B':
         m_tipus = TIPUS_NORMAL;
         m_color = COLOR_BLANC;
-        }
-        else 
-            if (c == 'N') 
-            {
-                m_tipus = TIPUS_NORMAL;
-                m_color = COLOR_NEGRE;
-            }
-            else 
-                if (c == 'D') 
-                {
-                    m_tipus = TIPUS_DAMA;
-                    m_color = COLOR_BLANC;
-                }
-                 else 
-                    if (c == 'R') 
-                    {
-                        m_tipus = TIPUS_DAMA;
-                        m_color = COLOR_NEGRE;
-                    }
+        break;
+
+    case 'N':
+        m_tipus = TIPUS_NORMAL;
+        m_color = COLOR_NEGRE;
+        break;
+
+    case 'D':
+        m_tipus = TIPUS_DAMA;
+        m_color = COLOR_BLANC;
+        break;
+
+    case 'R':
+        m_tipus = TIPUS_DAMA;
+        m_color = COLOR_NEGRE;
+        break;
+
+    default:
+        m_tipus = TIPUS_EMPTY;
+        m_color = COLOR_BLANC;
+        break;
+    }
 }
