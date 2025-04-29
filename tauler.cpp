95596e1752c58637569761a2521df3e0682b365a
NIU1746645 <1746645@uab.cat>
2025-04-29 14:17:06 +0200

    Update tauler.cpp

--- tauler.cpp
+++ tauler.cpp
@@ -1,5 +1,6 @@
 #include "tauler.hpp"
 
+//REVISIO
 void Tauler::inicialitzaTauler(char tauler[N_FILES][N_COLUMNES])
 {
     for (int i = 0; i < N_FILES; i++)
@@ -7,10 +8,12 @@ void Tauler::inicialitzaTauler(char tauler[N_FILES][N_COLUMNES])
         for (int j = 0; j < N_COLUMNES; j++)
         {
             tauler[i][j] = ' ';
+			//Mirar si cal inicialitzar el valor de fitxes una per una
         }
     }
 }
 
+//REVISIO
 void Tauler::llegeixTauler(const string& nomFitxer, char tauler[N_FILES][N_COLUMNES])
 {
     inicialitzaTauler(tauler);
@@ -31,9 +34,15 @@ void Tauler::llegeixTauler(const string& nomFitxer, char tauler[N_FILES][N_COLUM
 
         fitxer.close();
     }
+	else //COMPROVACIÓ D'OBERTURA DE FITXER
+    {
+        cout << "Error: No s'ha pogut obrir el fitxer " << nomFitxer << endl;
+        return;
+    }
 
 }
 
+//FET
 void Tauler::escriuTauler(const string& nomFitxer, char tauler[N_FILES][N_COLUMNES])
 {
     ofstream fitxer;
@@ -61,23 +70,56 @@ void Tauler::escriuTauler(const string& nomFitxer, char tauler[N_FILES][N_COLUMN
 
         fitxer.close();
     }
+	else //COMPROVACIÓ D'OBERTURA DE FITXER
+    {
+        cout << "Error: No s'ha pogut obrir el fitxer " << nomFitxer << endl;
+        return;
+    }
 }
 
+//Assegurar de que funcioni be
 void Tauler::inicialitza(const string& nomFitxer)
 {
+    ifstream fitxer;
+    fitxer.open(nomFitxer);
 
+    if (fitxer.is_open())
+    {
+        while (!fitxer.eof())
+        {
+            char fitxa;
+            string pos;
+            Posicio posicio;
+            fitxer >> fitxa;
+            fitxer >> pos;
+            //tractem dades
+			posicio.fromString(pos);
+			m_tauler[posicio.getFila()][posicio.getColumna()].fromChar(fitxa);
+        }
+        fitxer.close();
+    }
+	else //COMPROVACIÓ D'OBERTURA DE FITXER
+    {
+		cout << "Error: No s'ha pogut obrir el fitxer " << nomFitxer << endl;
+        return;
+    }
 }
 
 void Tauler::actualitzaMovimentsValids()
 {
-
+	//Implementar la lògica per actualitzar els moviments vàlids
 }
 
 bool Tauler::mouFitxa(const Posicio& origen, const Posicio& desti)
 {
-    return false;
+	//Comprovar si el moviment es valid
+
+    /*bool mValid = false;
+	m_tauler[origen.getColumna()][origen.getColumna()].setFitxa();
+    return mValid;*/
 }
 
+//FET
 void Tauler::getPosicionsPossibles(const Posicio& origen, int& nPosicions, Posicio posicionsPossibles[])
 {
     nPosicions = 0;
@@ -102,6 +144,7 @@ void Tauler::getPosicionsPossibles(const Posicio& origen, int& nPosicions, Posic
     }
 }
 
+//FET
 string Tauler::toString() const
 {
     return "";
