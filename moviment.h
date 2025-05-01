b79c75d23d5242a15f9cdb6534afbf2f21026e95
NIU1746645 <1746645@uab.cat>
2025-05-01 15:01:06 +0200

    Update moviment.h

--- moviment.h
+++ moviment.h
@@ -7,15 +7,16 @@ const int N_MOVIMENTS = 100;
 
 typedef enum
 {
-    MOV_AVANÇA,
-    MOV_MATA,
+	MOV_EMPTY, //Per defecte i s'enten que no pot fer cap moviment
+    MOV_AVANÇA, //No se si avança portara problemes i seria millor avanca
+	MOV_MATA, //
 } TipusMoviment;
 
 class Moviment
 {
 public:
-    Moviment(): m_FitxesMata(0) {}
-    Moviment(const Posicio& origen, const Posicio& desti) : m_origen(origen), m_desti(desti), m_FitxesMata(0) {}
+    Moviment() : m_FitxesMata(MOV_EMPTY) {}
+    Moviment(const Posicio& origen, const Posicio& desti) : m_origen(origen), m_desti(desti), m_FitxesMata(MOV_EMPTY) {}
 
     Posicio getOrigen() const { return m_origen; }
     Posicio getDesti() const { return m_desti; }
@@ -24,6 +25,11 @@ public:
     void reinicia();
     void mostra() const;
     void afegirMoviment(const Posicio& moviment);
+	void afegirCaptura(const Posicio& captura);
+	void setTipusMoviment(int index, TipusMoviment tipus);
+	TipusMoviment getTipusMoviment(int index) const;
+	Posicio getMoviment(int index) const;
+	Posicio getCaptura(int index) const;
 
     int getNumMoviments() const { return m_numMoviments; }
     int getNumaptures() const { return m_numCaptures; }
@@ -34,16 +40,15 @@ private:
 
     Posicio m_origen;
     Posicio m_desti;
-	
+
     Posicio m_movimentsValids[N_MOVIMENTS];
-    Posicio m_movimentActual[N_MOVIMENTS]; 
-	TipusMoviment m_tipusMoviment[N_MOVIMENTS]; //Si mata o avança
+    Posicio m_movimentActual[N_MOVIMENTS];
+    TipusMoviment m_tipusMoviment[N_MOVIMENTS]; //Si mata o avança
     int m_FitxesMata;
-	//Saber quin tipus de fitxa mata
+    //Saber quin tipus de fitxa mata
     Posicio m_posicionValides[N_MOVIMENTS];
     Posicio m_captures[N_MOVIMENTS];
 };
 
 #endif // MOVIMENT_H
-
 
