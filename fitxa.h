f31f891d7165cdd91f72af883400e97346c571d5
NIU1746645 <1746645@uab.cat>
2025-05-01 02:08:35 +0200

    Update fitxa.h

--- fitxa.h
+++ fitxa.h
@@ -2,6 +2,7 @@
 #define FITXA_H
 
 #include "posicio.hpp"
+#include "moviment.h"
 
 typedef enum
 {
@@ -19,21 +20,30 @@ typedef enum
 class Fitxa
 {
 public:
-    Fitxa() : m_tipus(TIPUS_EMPTY), m_color(COLOR_BLANC) {}
-    Fitxa(TipusFitxa tipus, ColorFitxa color) : m_tipus(tipus), m_color(color) {} 
+    Fitxa() : m_tipus(TIPUS_EMPTY), m_color(COLOR_BLANC) {};
+    Fitxa(TipusFitxa tipus, ColorFitxa color) : m_tipus(tipus), m_color(color) {};
 
     void setTipus(TipusFitxa tipus) { m_tipus = tipus; }
     void setColor(ColorFitxa color) { m_color = color; } 
     TipusFitxa getTipus() const { return m_tipus; } 
     ColorFitxa getColor() const { return m_color; }
-
+	
     bool esBuida() const { return m_tipus == TIPUS_EMPTY; }
     char toChar() const;
     void fromChar(char c);
 
+    void inicialitzaMovimentsValids(const Moviment& pos);
+    void inicialitzaMovimentsPendents(const Moviment& pos);
+    void inicialitzaMovimentActual(const Moviment& pos);
+    void recuperaPosicionsValides(const Moviment& pos);
+
 private:
     TipusFitxa m_tipus;
     ColorFitxa m_color;
+    Moviment m_movimentsValids[N_MOVIMENTS];
+    Moviment m_movimentsPendents[N_MOVIMENTS];
+    Moviment m_movimentActual[N_MOVIMENTS];
+    Moviment m_posicionsValides[N_MOVIMENTS];
 };
 
 #endif // FITXA_H
