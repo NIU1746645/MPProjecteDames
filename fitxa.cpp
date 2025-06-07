75124fceecf993fe7e9bf92f658a0993bee91f06
NIU1746645 <1746645@uab.cat>
2025-06-07 22:55:11 +0200

    Update fitxa.cpp

--- fitxa.cpp
+++ fitxa.cpp
@@ -1,5 +1,32 @@
 #include "fitxa.h"
 
+// Fem la promocio d'una fitxa normal a dama
+void Fitxa::promocioADama()
+{
+    if (m_tipus == TIPUS_NORMAL)
+    {
+        m_tipus = TIPUS_DAMA;
+    }
+}
+
+//Afegim un moviment valid a la fitxa
+void Fitxa::afegeixMovimentValid(const Moviment& mov)
+{
+    if (mov.esValid())
+    {
+        m_movimentsValids.push_back(mov);
+		m_nMovimentsValids++;
+    }
+}
+
+//Neteja tots els moviments
+void Fitxa::buidaMoviments() 
+{ 
+    m_movimentsValids.clear(); 
+    m_nMovimentsValids = 0; 
+}
+
+//Per passar del codi a caracters per fer el tauler
 char Fitxa::toChar() const
 {
     char tornar = ' ';
@@ -28,11 +55,11 @@ char Fitxa::toChar() const
     return tornar;
 }
 
+//Per pasar els caracters del tauler a codi
 void Fitxa::fromChar(char c)
 {
     switch (c)
     {
-
     case ' ':
         m_tipus = TIPUS_EMPTY;
         m_color = COLOR_BLANC;
