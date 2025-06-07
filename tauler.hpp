c0a89c0d8be2277e504b1e5feefc3a616260c7cd
NIU1746645 <1746645@uab.cat>
2025-06-07 22:57:44 +0200

    Update tauler.hpp

--- tauler.hpp
+++ tauler.hpp
@@ -1,25 +1,48 @@
-#ifndef TAULER_HPP
-#define TAULER_HPP
+#ifndef TAULER_H
+#define TAULER_H
 
-#include "posicio.hpp"
 #include "fitxa.h"
+#include "posicio.hpp"
+#include "moviment.h"
+#include <vector>
+#include <string>
+
+using namespace std;
+
+const int MAX_SALTS = 10;
+const int DIRECCIO_DAMES = 0;
+const int FILA_BLANCA = 0;
+const int FILA_NEGRA = 7;
 
 class Tauler
 {
 public:
-	void inicialitzaTauler(char tauler[N_FILES][N_COLUMNES]);
-	void llegeixTauler(const string& nomFitxer, char tauler[N_FILES][N_COLUMNES]);
-	void escriuTauler(const string& nomFitxer, char tauler[N_FILES][N_COLUMNES]);
+    Tauler();
+    void inicialitza(const string& nomFitxer);
+    void inicialitzaTauler(char tauler[N_FILES][N_COLUMNES]);
+    void llegeixTauler(const string& nomFitxer, char tauler[N_FILES][N_COLUMNES]);
+    void escriuTauler(const string& nomFitxer, char tauler[N_FILES][N_COLUMNES]);
 
-	void inicialitza(const string& nomFitxer);
-	void actualitzaMovimentsValids();
-	void getPosicionsPossibles(const Posicio& origen, int& nPosicions, Posicio posicionsPossibles[]);
-	bool mouFitxa(const Posicio& origen, const Posicio& desti);
-	string toString() const;
-	bool cercaPosicio(Posicio posicio[], Posicio novaPos);
+    string toString() const;
+    bool dinsDimTauler(const Posicio& pos) const;
+    bool esBuida(const Posicio& pos) const;
+    ColorFitxa getColorContrari(ColorFitxa color) const;
+    void afegirMoviment(Fitxa& fitxa, Moviment& mov, Posicio cami);
+    void afegirMovimentAmbCaptura(Fitxa& fitxa, Moviment& mov, Posicio cami, Posicio captures);
+
+    void movimentsNormals(Fitxa& fitxa, const Posicio& pos);
+    void movimentsDama(Fitxa& fitxa, const Posicio& pos);
+    void saltsMultiples(Fitxa& fitxa, Moviment& movimentActual, const Posicio& posActual, int direccio = 0);
+    
+    void actualitzaMovimentsValids();
+    bool mouFitxa(const Posicio& origen, const Posicio& desti);
+    void getPosicionsPossibles(const Posicio& origen, int& nPosicions, Posicio posicionsPossibles[]);
 
 private:
-	Fitxa m_tauler[N_FILES][N_COLUMNES];
+    Fitxa m_tauler[N_FILES][N_COLUMNES];
+
+
+    
 };
 
-#endif // TAULER_HPP
+#endif
